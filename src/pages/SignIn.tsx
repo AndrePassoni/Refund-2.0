81e20d8de02e0cf0fb7815516b4a305d3a21162f
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-16 19:27:40 -0300

    feat: implement Button component and update SignIn form to use it

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 
 export function SignIn() {
@@ -5,6 +6,8 @@ export function SignIn() {
         <form className="w-full flex flex-col gap-4">
             <Input required legend="E-mail" type="email" placeholder="seu@email.com" />
             <Input required legend="Senha" type="password" placeholder="123456" />
+
+            <Button>Entrar</Button>
         </form>
     )
 }

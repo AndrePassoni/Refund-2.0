cbc40199fc89fe99f0d9bb2e9dc8fe30f37130ea
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-17 16:11:20 -0300

    feat: add Select component and integrate category selection in Refund form

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,6 +1,11 @@
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/Categories";
 import { Input } from "../components/Input";
+import { Select } from "../components/Select";
+import { useState } from "react";
 
 export function Refund() {
+    const [category, setCategory] = useState("")
+
     return (
         <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-lg">
             <header>
@@ -12,6 +17,15 @@ export function Refund() {
                 </p>
 
                 <Input required legend="Nome da solicitação" />
+                <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+                    {
+                        CATEGORIES_KEYS.map((category) => (
+                            <option key={category} value={category}>
+                                {CATEGORIES[category].name}
+                            </option>
+                        ))
+                    }
+                </Select>
             </header>
         </form>
     )

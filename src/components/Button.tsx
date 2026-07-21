42475ff9ebf9e4296f276c1353a464c2b52ecacb
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-21 20:44:39 -0300

    feat: add Button component with variant support and integrate into Dashboard for search functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,28 @@
+import { classMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean
+    variant?: "base" | "icon" | "iconSmall"
 }
 
-export function Button({ children, isLoading, type = "button", ...rest }: Props) {
+const variants = {
+    button: {
+        base: "h-12",
+        icon: "h-12 w-12",
+        iconSmall: "h-10 w-10",
+    }
+}
+
+export function Button({ children, isLoading, className, type = "button", variant = "base",...rest }: Props) {
     return (
         <button 
             type={type} 
             disabled={isLoading} 
-            className="flex h-12 w-full items-center justify-center rounded-lg bg-green-100 text-white cursor-pointer transition-colors ease-linear hover:bg-green-200 disabled:cursor-not-allowed disabled:opacity-50" 
+            className={
+                classMerge(["flex items-center justify-center rounded-lg bg-green-100 text-white cursor-pointer transition-colors ease-linear hover:bg-green-200 disabled:cursor-not-allowed disabled:opacity-50",
+                variants.button[variant],
+                className
+                ])}
             {...rest}
         >
             {children}

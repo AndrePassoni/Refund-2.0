eb592336219acdf34e694470db4b87a809eaa524
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-07-22 15:13:43 -0300

    feat: add Loading component and integrate it into Routes for loading state handling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,18 @@ import { AuthRoutes } from "./auth-routes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
+import { Loading } from "../components/Loading";
+
+const isLoading = false
+
 export function Routes() {
+    if(isLoading) {
+        return <Loading />
+    }
+
     return (
         <BrowserRouter>
-            <ManagerRoutes />
+            <AuthRoutes />
         </BrowserRouter>
     )
 }
